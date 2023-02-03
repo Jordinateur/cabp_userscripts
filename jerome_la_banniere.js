81567108e7cc5423e656d9f442bcd22a621a40d2
Jordinateur <Jordinateur@users.noreply.github.com>
2023-02-03 09:33:39 +0100

    Typo fix

--- jerome_la_banniere.js
+++ jerome_la_banniere.js
@@ -2,7 +2,7 @@
 // @name         Jerome la bannière
 // @namespace    http://tampermonkey.net/
 // @downloadURL  https://github.com/Jordinateur/cabp_userscripts/raw/master/jerome_la_banniere.js
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.credit-agricole.fr/ca-briepicardie/*
@@ -52,7 +52,7 @@ function showImageInput(){
             for(var i = 0; i < banner_elements.length; i++){
                 var banner_element = banner_elements[i];
                 var change_me = document.createElement('button');
-                change_me.innerText = 'Changer moi...'
+                change_me.innerText = 'Changez-moi !'
                 change_me.style.position = 'absolute';
                 change_me.style.zIndex = '100';
                 change_me.onclick = () => {
